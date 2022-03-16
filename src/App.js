d5589466f0b655da38d4f69bd9f7fcd09c46e69b
Cleisson <cleissonconstc@gmail.com>
2022-03-16 15:58:19 -0300

    installin express cors and ytdl, and creating the server

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.scss';
 import { FaYoutube } from 'react-icons/fa';
-
+import { useState } from 'react';
 
 function App() {
+   const [URLinput, setURLinput] = useState('')
+
+   const sendURL = (URL) => {
+      // We will put code here later
+   }
+
+   const convertBtn = () => {
+      console.log(`URL: ${URLinput}`);
+      sendURL(URLinput);
+   };
+
    return (
       <div className="body">
          <h1 className="heading">My Own YouTube Downloader <FaYoutube /></h1>
-         <input className="URL-input" placeholder="Video URL e.g. https://www.youtube.com/watch?v=MtN1YnoL46Q" />
-         <button className="convert-button">Convert</button>
+         <input className="URL-input" onChange={e => setURLinput(e.target.value)} placeholder="Video URL e.g. https://www.youtube.com/watch?v=MtN1YnoL46Q" />
+         <button className="convert-button" onClick={convertBtn}>Convert</button>
       </div>
    );
 }

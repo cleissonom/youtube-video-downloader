8fe2beba6b8f1d91548ddc5e35d08856b23e0a08
Cleisson <cleissonconstc@gmail.com>
2022-03-16 15:29:40 -0300

    installing sass and using react-icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-
+import './App.scss';
+import { FaYoutube } from 'react-icons/fa';
 
 
 function App() {
    return (
       <div className="body">
-         <h1 className="heading">My Own YouTube Downloader</h1>
+         <h1 className="heading">My Own YouTube Downloader <FaYoutube /></h1>
          <input className="URL-input" placeholder="Video URL e.g. https://www.youtube.com/watch?v=MtN1YnoL46Q" />
          <button className="convert-button">Convert</button>
       </div>
